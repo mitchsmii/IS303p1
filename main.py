f3cad10469290c5da5071bb96d2bebd18d5fb2bb
Carson51-IS <cars2005@byu.edu>
2025-02-26 10:27:33 -0700

    f3 started
    I started on the f3 and need to syncronize it to make it work

--- main.py
+++ main.py
@@ -4,11 +4,14 @@
 # imports f1 (dispalyGameIntro) and the global variable sUserName
 from displayGameIntro import displayGameIntro
 from f2 import Menu
+from f3 import fullGame
 
 # calls displayGameIntro (Function 1) to display game into and return sUserName
 sUserName = displayGameIntro()
 print(sUserName)
 
 # show menu and show what option user chose
-sMenuChoice = Menu()
+sMenuChoice = int(Menu())
 print(sMenuChoice)
+
+fullGame()
